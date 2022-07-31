d5c1a8153ad5d4601c5273dd0a851f8354f42ccd
ingfosbreak <ingfosbreak@outlook.com>
2022-07-31 15:37:38 +0700

    Use FXRouter

--- src/main/java/ku/cs/ProjectApplication.java
+++ src/main/java/ku/cs/ProjectApplication.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import com.github.saacsos.FXRouter;
 
 import java.io.IOException;
 
 public class ProjectApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ProjectApplication.class.getResource("project.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Project-Name");
-        stage.setScene(scene);
-        stage.show();
+        FXRouter.bind(this, stage, "OakCoding", 800, 600);
+        configRoute();
+        FXRouter.goTo("project");
+    }
+
+    private static void configRoute() {
+        String packageStr = "ku/cs/";
+        FXRouter.when("project",packageStr+"project.fxml");
     }
 
     public static void main(String[] args) {
