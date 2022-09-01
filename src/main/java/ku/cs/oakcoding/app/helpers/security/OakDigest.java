4d9aad4a339b2b85d11b6ab3478b7f69a5a89aee
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-09-02 05:22:21 +0700

    Digest, System defaults
    * Hashing API
    * System default properties

--- src/main/java/ku/cs/oakcoding/app/helpers/security/OakDigest.java
+++ src/main/java/ku/cs/oakcoding/app/helpers/security/OakDigest.java
@@ -1,8 +1,15 @@
+/**
+ * @file OakDigest.java
+ * @version 1.0.0-complete
+ */
+
 package ku.cs.oakcoding.app.helpers.security;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,39 +18,167 @@ import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * MessageDigest of a String message or content of a file.
+ */
 public abstract class OakDigest {
 
-    static final String DEFAULT_DIGEST_ALGORITHM;
+    protected static final String DEFAULT_ALGORITHM;
+
+    protected static final Charset DEFAULT_CHARSET;
 
     static {
-        DEFAULT_DIGEST_ALGORITHM = "SHA-1";
+        DEFAULT_ALGORITHM = "SHA-1";
+        DEFAULT_CHARSET = StandardCharsets.UTF_8;
     }
 
+    /**
+     * Gets the default hashing algorithm.
+     * 
+     * @return      the default hashing algorithm
+     */
     public static String getDefaultDigestAlgorithm() {
-        return DEFAULT_DIGEST_ALGORITHM;
+        return DEFAULT_ALGORITHM;
     }
 
-    public static String getDigestStringOf(String algorithm,
-                                           String filePathString) throws FileNotFoundException {
+    /**
+     * Gets the default charset.
+     * 
+     * @return      the default charset encoding
+     */
+    public static Charset getDefaultCharset() {
+        return DEFAULT_CHARSET;
+    }
 
-        Path path = FileSystems.getDefault().getPath(filePathString);
+    /**
+     * Converts an array of byte to a String.
+     * 
+     * @param       bytes   array of byte
+     * 
+     * @return      the converted String
+     * 
+     * @note INTERNAL
+     */
+    protected static String byteArrayToHexString(byte[] bytes) {
+        String result = "";
 
-        if (Files.exists(path) == false) {
-            throw new FileNotFoundException(path.toString());
+        for (int i = 0, len = bytes.length; i < len; i++) {
+            result += Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1);
         }
 
-        String fileDigestString = "";
+        return result;
+    }
 
-        try (InputStream fis = Files.newInputStream(path, StandardOpenOption.READ)) {
-            MessageDigest fileDigest = MessageDigest.getInstance(DEFAULT_DIGEST_ALGORITHM);
+    /**
+     * Performs the hash computation on a String message.
+     * 
+     * @param       algorithm   Hasing algorithm to be used
+     * @param       charset     Decodes the  message into a sequence of bytes using the given @link java.nio.charset.Charset charset @endlink
+     * @param       message     message to be digest
+     * 
+     * @return      the message digest
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     */
+    public static String getDigestStringOfMessage(String algorithm,
+                                           Charset charset,
+                                           String message) throws NoSuchAlgorithmException {
+        String result = new String();
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
+            byte[] digestResultByte = messageDigest.digest(message.getBytes(charset));
+            result = byteArrayToHexString(digestResultByte);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    /**
+     * Performs the hash computation on a String message using the default algorithm.
+     * 
+     * @param       charset     Decodes the  message into a sequence of bytes using the given @link java.nio.charset.Charset charset @endlink
+     * @param       message     message to be digest
+     * 
+     * @return      the message digest
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * 
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultDigestAlgorithm()
+     */
+    public static String getDigestStringOfMessage(Charset charset,
+                                           String message) throws NoSuchAlgorithmException {
+        return getDigestStringOfMessage(DEFAULT_ALGORITHM,
+                                        charset,
+                                        message);
+    }
+
+    /**
+     * Performs the hash computation on a String message using the default charset.
+     * 
+     * @param       algorithm   Hasing algorithm to be used
+     * @param       message     message to be digest
+     * 
+     * @return      the message digest
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * 
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultCharset()
+     */
+    public static String getDigestStringOfMessage(String algorithm,
+                                           String message) throws NoSuchAlgorithmException {
+        return getDigestStringOfMessage(algorithm,
+                                        DEFAULT_CHARSET,
+                                        message);
+    }
+
+    /**
+     * Performs the hash computation on a String message using the default algorithm and default charset.
+     * 
+     * @param       message     message to be digest
+     * 
+     * @return      the message digest
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * 
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultDigestAlgorithm()
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultCharset()
+     */
+    public static String getDigestStringOfMessage(String message) throws NoSuchAlgorithmException {
+        return getDigestStringOfMessage(DEFAULT_ALGORITHM,
+                                        DEFAULT_CHARSET,
+                                        message);
+    }
+
+    /**
+     * Performs the hash computation on content of the given Path.
+     * 
+     * @param       filePath    Path to a file on the current file system
+     * 
+     * @return      the message digest of the given Path
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * @throws      FileNotFoundException       If the given Path points to file that does not exist
+     */
+    public static String getDigestStringOf(String algorithm,
+                                    Path filePath) throws NoSuchAlgorithmException, FileNotFoundException {
+        if (Files.exists(filePath) == false) {
+            throw new FileNotFoundException(filePath.toString());
+        }
+
+        String fileDigestString = new String();
+
+        try (InputStream fis = Files.newInputStream(filePath, StandardOpenOption.READ)) {
+            MessageDigest fileDigest = MessageDigest.getInstance(algorithm);
             DigestInputStream dis = new DigestInputStream(fis, fileDigest);
 
-            while (dis.read() != -1) ;
+            while (dis.read() != -1);
 
             byte[] digestBytes = fileDigest.digest();
 
-            for (int i = 0, len = digestBytes.length; i < len; i++)
-                fileDigestString += Integer.toString((digestBytes[i] & 0xff) + 0x100, 16).substring(1);
+            fileDigestString = byteArrayToHexString(digestBytes);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
@@ -53,16 +188,58 @@ public abstract class OakDigest {
         return fileDigestString;
     }
 
-    public static String getDigestStringOf(String filePathString) throws FileNotFoundException {
-        return getDigestStringOf(DEFAULT_DIGEST_ALGORITHM, filePathString);
+    /**
+     * Performs the hash computation on content of the given Path using the default algorithm.
+     * 
+     * @param       filePath    Path to a file on the current file system
+     * 
+     * @return      the message digest of the given Path
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * @throws      FileNotFoundException       If the given Path points to file that does not exist
+     * 
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultDigestAlgorithm()
+     */
+    public static String getDigestStringOf(Path filePath) throws NoSuchAlgorithmException, FileNotFoundException {
+        if (Files.exists(filePath) == false) {
+            throw new FileNotFoundException(filePath.toString());
+        }
+
+        return getDigestStringOf(DEFAULT_ALGORITHM,
+                                 filePath);
     }
 
+    /**
+     * Performs the hash computation on content of the given file.
+     * 
+     * @param       algorithm       Hasing algorithm to be used
+     * @param       filePathString  path String pointing to a file on the current file system
+     * 
+     * @return      the message digest of a file pointed to by the given path String
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * @throws      FileNotFoundException       If the given file pointed to by the given path String does not exist
+     * 
+     */
     public static String getDigestStringOf(String algorithm,
-                                           Path filePath) throws FileNotFoundException {
-        return getDigestStringOf(algorithm, filePath.toString());
+                                           String filePathString) throws NoSuchAlgorithmException, FileNotFoundException {
+        return getDigestStringOf(FileSystems.getDefault().getPath(filePathString));
     }
 
-    public static String getDigestStringOf(Path filePath) throws FileNotFoundException {
-        return getDigestStringOf(DEFAULT_DIGEST_ALGORITHM, filePath);
+    /**
+     * Performs the hash computation on content of the given file using the default algorithm.
+     * 
+     * @param       filePathString  path String pointing to a file on the current file system
+     * 
+     * @return      the message digest of a file pointed to by the given path String
+     * 
+     * @throws      NoSuchAlgorithmException    If the specified algorithm does not exist
+     * @throws      FileNotFoundException       If the given file pointed to by the given path String does not exist
+     * 
+     * @see         ku.cs.oakcoding.app.helpers.security.OakDigest#getDefaultDigestAlgorithm()
+     */
+    public static String getDigestStringOf(String filePathString) throws NoSuchAlgorithmException, FileNotFoundException {
+        return getDigestStringOf(DEFAULT_ALGORITHM,
+                                 filePathString);
     }
 }
