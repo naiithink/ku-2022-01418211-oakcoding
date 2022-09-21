4e346afc500e1b0bd5dd1e2ba61a08197abfb9bb
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-09-21 12:51:42 +0700

    Refactor project module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,29 @@
 module oakcoding.app {
+    requires java.desktop;
     requires java.logging;
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
 
-    // ku.cs.oakcoding.ProjectApplication.launch()
-    exports ku.cs.oakcoding to javafx.graphics;
+    /* 
+     * javafx.graphics is transitively required by javafx.controls
+     * 
+     * - ku.cs.oakcoding.ProjectApplication.launch()
+     */
+    exports ku.cs.oakcoding                     to  javafx.graphics;
 
-    // ku.cs.oakcoding.ProjectController
-    opens ku.cs.oakcoding to javafx.fxml;
+    /* 
+     * ku.cs.oakcoding.ProjectController
+     */
+    opens   ku.cs.oakcoding                     to  javafx.fxml;
 
-    // ku.cs.oakcoding.app.controllers.*
-    opens ku.cs.oakcoding.app.controllers to javafx.fxml;
+    /* 
+     * ku.cs.oakcoding.app.controllers.*
+     */
+    opens   ku.cs.oakcoding.app.controllers     to  javafx.fxml;
 
-    // com.github.saacsos.fxrouter
-    opens com.github.saacsos.fxrouter to javafx.fxml;
+    /* 
+     * com.github.saacsos.fxrouter
+     */
+    opens   com.github.saacsos.fxrouter         to  javafx.fxml;
 }
