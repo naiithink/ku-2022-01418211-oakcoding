61123cdba511bd5cd75a0091bca53c963b5357c0
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-10-20 16:22:35 +0700

    feat: Colon-separated list serializer

--- src/main/java/ku/cs/oakcoding/app/helpers/hotspot/OakHotspot.java
+++ src/main/java/ku/cs/oakcoding/app/helpers/hotspot/OakHotspot.java
@@ -1,6 +1,13 @@
 package ku.cs.oakcoding.app.helpers.hotspot;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 public final class OakHotspot {
     private OakHotspot() {}
@@ -25,5 +32,37 @@ public final class OakHotspot {
                 return false;
 
         return true;
+    }
+
+    public static List<String> separateColonValues(String message) {
+        if (Objects.isNull(message))
+            return null;
+
+        List<String> res = new ArrayList<>();
+        String[] resArray = message.split(":");
+
+        for (String elem : resArray) {
+            res.add(elem);
+        }
+
+        return res;
+    }
+
+    public static String toColonSeparatedValues(Collection<String> collection) {
+        if (Objects.isNull(collection))
+            return null;
+
+        StringBuffer resBuf = new StringBuffer();
+
+        Iterator<String> elemIter = collection.iterator();
+
+        while (elemIter.hasNext()) {
+            resBuf.append(elemIter.next());
+
+            if (elemIter.hasNext())
+                resBuf.append(":");
+        }
+
+        return resBuf.toString();
     }
 }
